e6047d9b057e6ea5c852bcf317525b45049b9bf5
Ayushkaushal <ayushk4@gmail.com>
2018-07-05 10:54:48 +0530

    Fix bugs

--- MakeGraphs.py
+++ MakeGraphs.py
@@ -1,28 +1,19 @@
 import matplotlib.pyplot as plt
 import sys
-from PIL import Image
 import os
-from io import StringIO
-from StringIO import StringIO
-
+from os import listdir
+from PIL import Image
+import shutil
 
 #val = {'2017Autumn': {'A': 12, 'C': 2, 'B': 3, 'D': 0, 'F': 0, 'P': 0, 'EX': 8}, '2018Spring': {'A': 6, 'C': 3, 'B': 6, 'D': 3, 'F': 0, 'P': 3, 'EX': 4}, '2016Spring': {'A': 2, 'C': 0, 'B': 1, 'D': 0, 'F': 0, 'P': 0, 'EX': 3}, '2017Spring': {'A': 12, 'C': 2, 'B': 3, 'D': 0, 'F': 0, 'P': 0, 'EX': 8}}
 
-def MakeGraphs(val) :
-    numberOfImage=0
-    for semester, grades in val.iteritems():
-        x_groups = ['EX', 'A', 'B', 'C', 'D', 'P', 'F']
-        y_values = [grades['EX'],grades['A'],grades['B'],grades['C'],grades['D'],grades['P'],grades['F']]
-        plt.bar(x_groups,y_values)
-        plt.title(semester)
-        plt.ylabel('No of students')
-        plt.savefig('Grades/Temp_files/%s.png' % semester)
-        plt.close()
-        numberOfImage = numberOfImage+1
+def RemovePreviousImage():
+    mydir = 'figure/'
+    filelist = [ f for f in os.listdir(mydir) if f.endswith(".jpg") ]
+    for f in filelist:
+        os.remove(os.path.join(mydir, f))
 
-    CombineImage(val)
-
-def CombineImage(val):
+def CombineImage(val, code):
     list_graphs = []
     for semester, grades in val.iteritems():
         list_graphs.append('Grades/Temp_files/%s.png' % semester)
@@ -38,19 +29,26 @@ def CombineImage(val):
         new_im.paste(im, (x_offset,0))
         x_offset += im.size[0]
 
-    new_im.save('figure/combinedGrades.jpg')
+    new_im.save('figure/%s.jpg' % code)
 
     for deleteImage in list_graphs:
         os.remove('%s' % deleteImage)
-        
-  #  img = StringIO()
- #   new_im.save(img)
-#    img.seek(0)
 
-    #return (img)
+
+def MakeGraphs(val, code) :
+    numberOfImage=0
+    for semester, grades in val.iteritems():
+        x_groups = ['EX', 'A', 'B', 'C', 'D', 'P', 'F']
+        y_values = [grades['EX'],grades['A'],grades['B'],grades['C'],grades['D'],grades['P'],grades['F']]
+        plt.bar(x_groups,y_values)
+        plt.title(semester)
+        plt.ylabel('No of students')
+        plt.savefig('Grades/Temp_files/%s.png' % semester)
+        plt.close()
+        numberOfImage = numberOfImage+1
+
+    RemovePreviousImage()
+    CombineImage(val, code)
+
 
 #MakeGraphs(val)
-    
-
-
-
